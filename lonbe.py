8f54b2a718a26cc09f21749ebca2dce9b961195c
NhamNgaHanh <40319652+NhamNgaHanh@users.noreply.github.com>
2024-07-01 13:03:42 +0900

    Update lonbe.py

--- lonbe.py
+++ lonbe.py
@@ -115,8 +115,8 @@ with col2:
         else:
             be = be + 1
     if tl != 0:
-        st.subheader(f":red[Tỉ lệ ra lớn là: {round((lon/tl)*100,2)}]")
-        st.subheader(f":red[Tỉ lệ ra bé là: {round((be / tl) * 100, 2)}]")
+        st.subheader(f":red[Tỉ lệ ra lớn là: {round((lon/tl)*100,2)}%]")
+        st.subheader(f":red[Tỉ lệ ra bé là: {round((be / tl) * 100, 2)}%]")
         #st.write( round((lon/tl)*100,2))
         #st.write( round((be / tl) * 100, 2))
     num_r = []
